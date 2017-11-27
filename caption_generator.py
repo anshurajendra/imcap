b7c455f643777ba52419cd17f23a3bfe9c5c1436
anshurajendra <anshurajendra@gmail.com>
2017-11-27 01:14:57 -0800

    basic changes

--- caption_generator.py
+++ caption_generator.py
@@ -137,7 +137,8 @@ class CaptionGenerator():
         image_model.add(RepeatVector(20))
 
         lang_model = Sequential()
-        lang_model.add(Dense(EMBEDDING_DIM, input_shape=(20,200)))
+        lang_model.add(Embedding(200, EMBEDDING_DIM, input_length=20))
+        #lang_model.add(Dense(EMBEDDING_DIM, input_shape=(20,200)))
         lang_model.add(TimeDistributed(Dense(EMBEDDING_DIM)))
 
         model = Sequential()
